8f45be538bfc92ab22ea3e5b80f6237d52c4b901
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-29 08:38:28 +0700

    Navbar Scroll Effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // Add custom JavaScript here
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('bg-dark')
+    navbar.classList.add('navbar-sticky')
+  } else {
+    navbar.classList.remove('bg-dark')
+    navbar.classList.remove('navbar-sticky')
+  }
+})
