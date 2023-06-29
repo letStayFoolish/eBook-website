c5e4e5f0f6ed28e173bebcb76c47a9203ebdc143
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-06-29 14:48:33 +0700

    Contact Page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 // Add custom JavaScript here
-const navbar = document.querySelector('.navbar')
+function useScroll () {
+  const navbar = document.querySelector('.navbar')
+  const navbarNavDropdown = document.getElementById('navbarNavDropdown')
+  const btn = document.querySelector('.navbar-toggler')
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    navbar.classList.add('bg-dark')
-    navbar.classList.add('navbar-sticky')
-  } else {
-    navbar.classList.remove('bg-dark')
-    navbar.classList.remove('navbar-sticky')
-  }
-})
+  // btn.addEventListener('click',() => navbar.classList.toggle('bg-dark'))
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark')
+      navbar.classList.add('navbar-sticky')
+    } else {
+      navbar.classList.remove('bg-dark')
+      navbar.classList.remove('navbar-sticky')
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded',useScroll)
